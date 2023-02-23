d8fecf00a07ab7c8802825655d5b3d3f566319b6
Ty Garber <tyler.garber@dfw.wa.gov>
2023-02-23 10:39:57 -0800

    fix to area 5 needlessly pulling in crc data, chart comparing input to historical catch and pre-season inputs

--- data_prep_scripts/compile_to_database.R
+++ data_prep_scripts/compile_to_database.R
@@ -8,7 +8,7 @@ source(here::here('data_prep_scripts/coho_estimates.R'))
 
 
 # comments on the data associated with this update
-comment <- 'crc days without catch area now 0'
+comment <- 'fix to area 5, needlessly pulling in CRC data'
 
 # save to sqlite database
 con <- DBI::dbConnect(RSQLite::SQLite(), here::here('data/coho_harvest_estimator.db'))
