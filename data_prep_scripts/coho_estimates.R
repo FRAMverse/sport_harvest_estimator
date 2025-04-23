dc9a996d43dd1e3cb0263c1f488f716d514750d1
cbedwards-dfw <155684316+cbedwards-dfw@users.noreply.github.com>
2025-04-23 15:57:45 -0700

    2025 updates (#11)
    * fixed calculation of days for hpd, updated 0-adder, added days spanned to shiny
    
    * Updating ts 1 defaults to end at the end of april
    
    * updated to automatically use the right years with

--- data_prep_scripts/coho_estimates.R
+++ data_prep_scripts/coho_estimates.R
@@ -34,10 +34,15 @@ print('Filling in missing days in CRCs with 0s')
 
 all_area_days <- expand_grid(
   area = c("05", "06", "07", "09", "10", "11", "12", "13", "81", "82"),
-  date = seq(as.Date('2002-01-01'), as.Date('2022-03-31'), by ='day'),
+  date = seq(as.Date('2002-01-01'), as.Date('2024-03-31'), by ='day'),
   species = 'Coho',
   fish = 0
 )
+
+print(paste0('Filling in zeroes from ', max(all_area_days$date),' to ',
+             min(all_area_days$date), '.'))
+print('MAKE SURE THAT FINAL DATE IS APPROPRIATE FOR CURRENT CRCs!!')
+
 
 missing_crc_days <- all_area_days %>%
   anti_join(crc, by=c('area', 'date')) %>%
