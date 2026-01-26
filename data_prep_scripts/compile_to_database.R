45219a163527b4ce5d6f7d23c2e1668cb15ef6ca
Garber <Tyler.Garber@dfw.wa.gov>
2026-01-26 12:35:26 -0800

    database

--- data_prep_scripts/compile_to_database.R
+++ data_prep_scripts/compile_to_database.R
@@ -8,7 +8,7 @@ source(here::here('data_prep_scripts/coho_estimates.R'))
 
 
 # comments on the data associated with this update
-comment <- 'Fixing missing Area 11 data'
+comment <- 'update for 2025 / 2026 pre-season'
 
 # save to sqlite database
 con <- DBI::dbConnect(RSQLite::SQLite(), here::here('data/coho_harvest_estimator.db'))
